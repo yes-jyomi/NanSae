d74bf65b1c02cc5c4fc7a87fede5fe7f48f7c05e
Sagittta <js1verj@gmail.com>
2020-04-09 15:24:37 +0900

    Add: Get uri '/', Redirect '/portfolio/list', Get uri '/template', Rendering template.ejs, Get uri '/list', Rendering portfolio.ejs, Get uri '/template/write', Rendering portfolioWrite.ejs, Post uri '/template/write'

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -2,4 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 
+res.get('/', function(req, res, next) {
+    res.redirect('/portfolio/list');
+});
+
+res.get('/template', function(req, res, next) {
+    res.render('template');
+});
+
+res.get('/list', function(req, res, next) {
+   res.render('portfolioList');
+});
+
+res.get('/template/write', function(req, res, next) {
+    res.render('portfolioWrite');
+});
+
+res.post('/template/write', function(req, res, next) {
+
+});
+
 module.exports = router;

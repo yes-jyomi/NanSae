5a9b8b908d7c37a0faa254371cd7811e2f5062bf
Sagittta <js1verj@gmail.com>
2020-04-09 15:20:11 +0900

    Add: Get uri '/', Redirect '/graph/score', Get uri '/score', Rendering score.ejs, Get uri '/score/write', Rendering writeScore.ejs, Post uri '/score/write', Get uri '/life', Rendering life.ejs, Get uri '/life/write', Rendering writeLife.ejs, Post uri '/life/write'

--- routes/graph.js
+++ routes/graph.js
@@ -2,4 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 
+router.get('/', function(req, res, next) {
+    res.redirect('/graph/score');
+});
+
+router.get('/score', function(req, res, next) {
+    res.render('score');
+});
+
+router.get('/score/write', function(req, res, next) {
+    res.render('writeScore');
+});
+
+router.post('/score/write', function(req, res, next) {
+
+});
+
+router.get('/life', function(req, res, next) {
+    res.render('life');
+});
+
+router.get('/life/write', function(req, res, next) {
+    res.render('writeLife');
+});
+
+router.post('/life/write', function(req, res, next) {
+
+});
+
 module.exports = router;

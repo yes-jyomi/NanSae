52e3a76064b8af0915fa348956514523664ba3d5
Sagittta <js1verj@gmail.com>
2020-04-09 15:22:51 +0900

    Add: Get uri '/', Rendering outdoorList.ejs, Get uri '/write', Rendering outdoorWrite.ejs, Post uri '/write', Get uri '/update', Rendering outdoorUpdate.ejs, Post uri '/update'

--- routes/outdoor.js
+++ routes/outdoor.js
@@ -2,4 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 
+res.get('/', function(req, res, next) {
+    res.render('outdoorList');
+});
+
+res.get('/write', function(req, res, next) {
+    res.render('outdoorWrite');
+});
+
+res.post('/write', function(req, res, next) {
+
+});
+
+res.get('/update', function(req, res, next) {
+    res.render('outdoorUpdate');
+});
+
+res.post('/update', function(req, res, next) {
+
+});
+
 module.exports = router;

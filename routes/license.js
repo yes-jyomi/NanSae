8be0440c435854a0b7ee97245e5b7d8244cc7673
Sagittta <js1verj@gmail.com>
2020-04-09 15:17:50 +0900

    Add: Get uri '/', Rendering licenseList.ejs, Get uri '/add', Rendering licenseAdd.ejs, Post uri '/add', Get uri '/update', Rendering licenseUpdate.ejs, Port uri '/update'

--- routes/license.js
+++ routes/license.js
@@ -2,4 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 
+res.get('/', function(req, res, next) {
+    res.render('licenseList');
+});
+
+res.get('/add', function(req, res, next) {
+    res.render('licenseAdd');
+});
+
+res.post('/add', function(req, res, next) {
+
+});
+
+res.get('/update', function(req, res, next) {
+    res.render('licenseUpdate');
+});
+
+res.post('/update', function(req, res, next) {
+
+});
+
 module. exports = router;

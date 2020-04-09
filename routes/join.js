de520ed9a769499a75ecb161fade23837062e0a6
Sagittta <js1verj@gmail.com>
2020-04-09 15:15:49 +0900

    Add: Get uri '/', Rendering join.ejs, Post uri '/'

--- routes/join.js
+++ routes/join.js
@@ -2,4 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 
+router.get('/', function(req, res, next) {
+    res.render('join');
+});
+
+router.post('/', function(req, res, next) {
+
+});
+
 module.exports = router;

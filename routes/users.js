ae4c3611a93176498f8b8761e72f3254b09d02eb
Sagittta <js1verj@gmail.com>
2020-04-23 21:25:46 +0900

    feat: Add join function

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql_odbc = require('../db/db_conn')();
+const conn = mysql_odbc.init();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.redirect('/mypage');
+  res.redirect('/users/mypage');
 });
 
 router.get('/mypage', function(req, res, next) {
@@ -15,7 +17,27 @@ router.get('/join', function(req, res, next) {
 });
 
 router.post('/join', function(req, res, next) {
+  const id = req.body.id;
+  const pwd = req.body.pwd;
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const zipcode = req.body.zipcode;
+  const address = req.body.address;
+  let blog = req.body.blog;
 
+  const empty = (blog) => {
+    if (blog === null) blog = "";
+  };
+
+  const datas = [id, pwd, name, phone, zipcode, address, email, blog];
+
+  const sql = "INSERT INTO user (user_id, user_pwd, user_name, user_phone, user_zipcode, user_address, user_email, user_blog)" +
+      "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  conn.query(sql, datas, function(err, rows) {
+    if (err) console.error("err: " + err);
+    res.redirect('/users/login');
+  });
 });
 
 router.get('/login', function(req, res, next) {

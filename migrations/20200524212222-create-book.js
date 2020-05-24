d21269198a135b9dd1640279391d9643d00479eb
Sagittta <js1verj@gmail.com>
2020-05-25 06:24:24 +0900

    feat: Add books database, migrate

---
+++ migrations/20200524212222-create-book.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('books', {
+      book_idx: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      book_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      book_comment: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('books');
+  }
+};

14ffedc34042b09499597dd5c5a56c25b1772e07
yes-jyomi <js1verj@gmail.com>
2020-05-27 15:50:48 +0900

    edit calendar

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  if (!req.session.user_id) res.redirect("/users/login");
+router.get('/', function (req, res, next) {
+  // if (!req.session.user_id) res.redirect("/users/login");
   res.render("calendarList", { user_id: req.session.user_id });
 });
 
-router.get("/write", function (req, res, next) {
+router.get('/write', function (req, res, next) {
   res.render("calendarWrite");
 });
 
